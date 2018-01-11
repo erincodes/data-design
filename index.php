773813038864154684a383db6c60ef647c1d853b
erincodes <erinleeannscott@gmail.com>
2018-01-11 13:50:28 -0700

    Added additional HTML details and created a CSS file.

--- index.php
+++ index.php
@@ -6,25 +6,35 @@
 		<title>Data Design</title>
 	</head>
 	<body>
-		<h1>Data Design</h1>
-		<img src = "images/dataDesign-markup.jpg" alt = "data design markup">
+		<h1><em>Data Design</em></h1>
+		<p>Persona</p>
+		<p>User Story</p>
+		<p>Use Case/Interaction Flow</p>
+		<p>Conceptual Model:</p>
+		<img src = "images/medium-entity-relationship-diagram.png" alt = "data design markup">
 		<br>
-		<h2><span id = "redColorTxt">Profile</span></h2>
+		<h2>Profile (entity)</h2>
 		<ul>
 			<li>profileName</li>
 			<li>profilePass</li>
 			<li>profileEmail</li>
 		</ul>
-		<h2><span id ="purpleColorTxt">Article</span></h2>
+		<h2>Article (entity)</h2>
 		<ul>
 			<li>articleName</li>
 			<li>articleId</li>
 			<li>articleContent</li>
 			<li>articleDate</li>
 		</ul>
+		<h2>Claps (entity)</h2>
+		<ul>
+			<li>numberOfClaps</li>
+			<li>numberOfUsersWhoClapped</li>
+		</ul>
 		<h2>Relationships</h2>
 		<ul>
 			<li>One profile can write many articles (1-to-n)</li>
 		</ul>
+		<br>
 	</body>
 </html>
