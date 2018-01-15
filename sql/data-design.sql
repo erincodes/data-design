dc51ade801aedd9f99f97d3efc44b45a6bf3ea0b
erincodes <erinleeannscott@gmail.com>
2018-01-15 16:34:01 -0700

    Updated data-design.sql to match my ERD

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -1,62 +1,50 @@
--- The statement below sets the collation of the database to utf8
-ALTER DATABASE your_database_name_CHANGE_ME CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+-- not sure if my database name "data_design" below is correct:
+ALTER DATABASE data_design CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
--- this is a comment in SQL (yes, the space is needed!)
--- these statements will drop the tables and re-add them
--- this is akin to reformatting and reinstalling Windows (OS X never needs a reinstall...) ;)
--- never ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever
--- do this on live data!!!!
-DROP TABLE IF EXISTS `like`;
-DROP TABLE IF EXISTS tweet;
-DROP TABLE IF EXISTS profile;
-
--- the CREATE TABLE function is a function that takes tons of arguments to layout the table's schema
 CREATE TABLE profile (
-	-- this creates the attribute for the primary key
-	-- not null means the attribute is required!
+	-- Our class Style Guide dictates that primary keys are listed first, foreign keys are listed second, and all other attributes are listed alphabetically. Refer to Style Guide here: https://bootcamp-coders.cnm.edu/study-guides/data-design-study-guide.php
+
+	-- The following profileId creates space for the attribute that will become the primary key, but we haven't done anything with it yet:
 	profileId BINARY(16) NOT NULL,
 	profileActivationToken CHAR(32),
-	profileAtHandle VARCHAR(32) NOT NULL,
 	profileEmail VARCHAR(128) NOT NULL,
-	-- to make something optional, exclude the not null
-	profileHash	CHAR(128) NOT NULL,
-	profilePhone VARCHAR(32),
+	profileHash CHAR(128) NOT NULL,
+	profileName VARCHAR(32) NOT NULL,
 	profileSalt CHAR(64) NOT NULL,
-	-- to make sure duplicate data cannot exist, create a unique index
-	UNIQUE(profileAtHandle),
+	-- Creating UNIQUE indexes to ensure duplicate data cannot exist
 	UNIQUE(profileEmail),
-	-- this officiates the primary key for the entity
+	UNIQUE(profileName),
+	-- The following officially makes the profileId attribute into the primary key:
 	PRIMARY KEY(profileId)
 );
 
--- create the tweet entity
-CREATE TABLE tweet (
-	-- this is for yet another primary key...
-	tweetId BINARY(16) NOT NULL,
-	-- this is for a foreign key
-	tweetProfileId BINARY(16) NOT NULL,
-	tweetContent VARCHAR(140) NOT NULL,
-	tweetDate DATETIME(6) NOT NULL,
-	-- this creates an index before making a foreign key
-	INDEX(tweetProfileId),
-	-- this creates the actual foreign key relation
-	FOREIGN KEY(tweetProfileId) REFERENCES profile(profileId),
-	-- and finally create the primary key
-	PRIMARY KEY(tweetId)
+CREATE TABLE article (
+	articleId BINARY(16) NOT NULL,
+	-- The following creates an attribute for what we'll later make into a foreign key; it references data from another table (from the profile table in this case):
+	articleProfileId BINARY(16) NOT NULL,
+	articleContent VARCHAR(200) NOT NULL,
+	articleDateTime DATETIME(6) NOT NULL,
+	articleTitle VARCHAR(32) NOT NULL,
+	-- Create an index before creating a foreign key. All foreign keys MUST be indexed, but don't necessarily have to be unique indexes (depending on the project):
+	INDEX(articleProfileId),
+	-- The following creates the actual foreign key relationship:
+	FOREIGN KEY(articleProfileId) REFERENCES profile(profileId),
+	-- Don't include a comma after the last field! It will cause an error message.
+	PRIMARY KEY(articleId)
 );
 
--- create the like entity (a weak entity from an m-to-n for profile --> tweet)
-CREATE TABLE `like` (
-	-- these are still foreign keys
-	likeProfileId BINARY(16) NOT NULL,
-	likeTweetId BINARY(16) NOT NULL,
-	likeDate DATETIME(6) NOT NULL,
-	-- index the foreign keys
-	INDEX(likeProfileId),
-	INDEX(likeTweetId),
-	-- create the foreign key relations
-	FOREIGN KEY(likeProfileId) REFERENCES profile(profileId),
-	FOREIGN KEY(likeTweetId) REFERENCES tweet(tweetId),
-	-- finally, create a composite foreign key with the two foreign keys
-	PRIMARY KEY(likeProfileId, likeTweetId)
+-- claps is a weak entity (or a try-hard)
+CREATE TABLE `claps` (
+	clapId BINARY(16) NOT NULL,
+	clapArticleId BINARY(16) NOT NULL,
+	clapProfileId BINARY(16) NOT NULL,
+	-- Index the foreign keys:
+	INDEX(clapArticleId),
+	INDEX(clapProfileId),
+	-- Create the foreign key relationships below:
+	FOREIGN KEY(clapArticleId) REFERENCES article(articleId),
+	FOREIGN KEY(clapProfileId) REFERENCES profile(profileId),
+	PRIMARY KEY(clapId)
+	-- Not sure if the primary key in this case should be a composite? In that case I think it'd be:
+	-- PRIMARY KEY(clapArticleId, clapProfileId)
 );
